ba218071ef570fb2a3774304380049692de3d21b
mand-ollar <minsec9912@icloud.com>
2024-03-22 12:04:07 +0900

    add send anywhere file

---
+++ send_anywhere.py
@@ -0,0 +1,34 @@
+import os
+import socket
+import pexpect
+
+## FIND LOCAL IP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+local_ip = s.getsockname()[0]
+s.close()
+
+## FIND OS INFO
+os_info = os.uname().sysname
+
+## IF OS IS LINUX OR MACOS
+if os_info == "Darwin":
+    pass
+elif os_info == "Linux":
+    pass
+else:
+    pass
+
+sending_from  = input("From:\t")
+sending_to = input("To:\t")
+print()
+send_what = input("What do u wanna send?: ")
+send_where = input("Where do u wanna send?: ")
+print()
+
+print("Sending...")
+if sending_from == "me":
+    os.system(f"rsync -Pr {send_what.strip()} {sending_to}:{send_where.strip()}")
+    
+elif sending_to == "me":
+    os.system(f"rsync -Pr {sending_from}:{send_what.strip()} {send_where.strip()}")
